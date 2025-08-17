fe99d82061cfbb9a53bab2ab271b814878ef6289
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2025-08-17 14:24:46 +0800

    feat: argparser

--- src/bugit_v2/app.py
+++ src/bugit_v2/app.py
@@ -1,11 +1,14 @@
+import argparse
 import os
 from dataclasses import dataclass
-from typing import final
+from typing import Literal, final
 
 from textual import work
 from textual.app import App, ComposeResult
 from textual.binding import Binding
+from textual.driver import Driver
 from textual.reactive import var
+from textual.types import CSSPathType
 from textual.widgets import Footer, Header, LoadingIndicator
 from typing_extensions import override
 
@@ -22,9 +25,23 @@ from bugit_v2.screens.submission_progress_screen import (
 
 @dataclass(slots=True)
 class AppState:
+    """The global app state.
+
+    Combination of null and non-nulls determine which state we are in
+    - All null: session selection
+    - Only session is NOT null: job selection
+    - Only bug_report is null: editor
+    - All non-null: submission in progress
+    """
+
     session: Session | None = None
     job_id: str | None = None
     bug_report: BugReport | None = None
+
+
+@dataclass(slots=True)
+class AppArgs:
+    submitter: Literal["lp", "jira"]
 
 
 @final
@@ -32,6 +49,17 @@ class BugitApp(App[None]):
     state = var(AppState())
     bug_report_backup: BugReport | None = None
     BINDINGS = [Binding("alt+left", "go_back", "Go Back")]
+
+    def __init__(
+        self,
+        args: AppArgs,
+        driver_class: type[Driver] | None = None,
+        css_path: CSSPathType | None = None,
+        watch_css: bool = False,
+        ansi_color: bool = False,
+    ):
+        self.args = args
+        super().__init__(driver_class, css_path, watch_css, ansi_color)
 
     @work
     async def on_mount(self) -> None:
@@ -123,8 +151,20 @@ class BugitApp(App[None]):
         yield Footer()
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("submitter", choices=["lp", "jira"])
+    return parser.parse_args()
+    # parser.add_argument("--submitter", choices=["lp", "jira"])
+
+
 def main():
-    app = BugitApp()
+    args = parse_args()
+    # vars() is very ugly, but it allows the AppArgs constructor to fail fast
+    # before the app takes over the screen
+    args = AppArgs(*vars(args).values())
+    print(args)
+    app = BugitApp(args)
     app.run()
 
 
