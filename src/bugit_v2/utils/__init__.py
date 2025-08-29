97b6a8b11fda8e4ca0e7675c2eb9b71e729da34a
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2025-08-29 07:44:00 +0000

    Move to core24 and use devmode (#19)

--- src/bugit_v2/utils/__init__.py
+++ src/bugit_v2/utils/__init__.py
@@ -4,3 +4,17 @@ import os
 def is_prod() -> bool:
     """Is bugit in a prod environment?"""
     return os.getenv("PROD") == "1"
+
+
+def on_ubuntucore() -> bool:
+    """
+    From checkbox: https://github.com/canonical/checkbox/blob/main/checkbox-ng/plainbox/impl/unit/unit.py#L61
+    """
+    snap = os.getenv("SNAP")
+    if snap:
+        with open(os.path.join(snap, "meta/snap.yaml")) as f:
+            for line in f.readlines():
+                if line == "confinement: classic\n":
+                    return False
+        return True
+    return False
