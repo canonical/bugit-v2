910bf4a973b107019333ade150da3c8d224bc25d
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2025-09-04 08:16:38 +0000

    Add Completers (#32)

---
+++ src/bugit_v2/bugit-completer.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+_bugit_v2_completion() {
+    local IFS=$'
+'
+    COMPREPLY=( $( env COMP_WORDS="${COMP_WORDS[*]}" \
+                   COMP_CWORD=$COMP_CWORD \
+                   _BUGIT_V2_COMPLETE=complete_bash python3 $SNAP/src/bugit_v2/app.py ) )
+    return 0
+}
+
+
+complete -o default -F _bugit_v2_completion bugit.bugit-v2
