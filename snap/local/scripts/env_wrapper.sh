572db4d30969ec103cff89839400c266ee0e1a70
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2025-09-09 10:10:45 +0800

    style: formatting

--- snap/local/scripts/env_wrapper.sh
+++ snap/local/scripts/env_wrapper.sh
@@ -3,21 +3,26 @@
 # this whole thing is here to export the PERL5LIB variable
 # originally from checkbox
 case "$SNAP_ARCH" in
-    "amd64") ARCH='x86_64-linux-gnu'
-    ;;
-    "i386") ARCH='i386-linux-gnu'
-    ;;
-    "arm64") ARCH='aarch64-linux-gnu'
-    ;;
-    "armhf") ARCH='arm-linux-gnueabihf'
-    ;;
-    *)
-        echo "Unsupported architecture: $SNAP_ARCH"
-    ;;
+"amd64")
+	ARCH='x86_64-linux-gnu'
+	;;
+"i386")
+	ARCH='i386-linux-gnu'
+	;;
+"arm64")
+	ARCH='aarch64-linux-gnu'
+	;;
+"armhf")
+	ARCH='arm-linux-gnueabihf'
+	;;
+*)
+	echo "Unsupported architecture: $SNAP_ARCH"
+	;;
 esac
 
 PERL_VERSION=$(perl -e '$^V=~/^v(\d+\.\d+)/;print $1')
 export PERL5LIB="$PERL5LIB:$SNAP/usr/lib/$ARCH/perl/$PERL_VERSION:$SNAP/usr/lib/$ARCH/perl5/$PERL_VERSION:$SNAP/usr/share/perl/$PERL_VERSION:$SNAP/usr/share/perl5"
 echo "Called wrapper!"
 
+# https://github.com/snapcrafters/get-iplayer/blob/candidate/snap/local/scripts/launcher
 exec "$@"
