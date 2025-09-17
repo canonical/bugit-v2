a6304de0b1dd87295fb36be7cf31eba9929fe9fd
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2025-09-17 05:39:07 +0000

    Fix journal collector and add slow collector watchers (#40)
    * fix: add timeout for all subprocess calls, add journal of past 3 days option
    
    * style: show the expected timeout
    
    * fix: name collision
    
    * feat: add a watcher timer to check the status of collectors
    
    * fix: capitalization
    
    * style: use human readable time in dmesg

--- src/bugit_v2/dut_utils/log_collectors.py
+++ src/bugit_v2/dut_utils/log_collectors.py
@@ -16,6 +16,8 @@ from typing import Callable
 
 from bugit_v2.models.bug_report import BugReport, LogName
 
+COMMAND_TIMEOUT = 10 * 60  # 10 minutes
+
 
 @dataclass(slots=True)
 class LogCollector:
@@ -35,6 +37,10 @@ class LogCollector:
     # provide a way for the user to manually collect the logs if this collector
     # failed at runtime
     manual_collection_command: str | None = None
+    # how long until this collector is expected to timeout
+    # this is purely visual and doesn't change any behaviors
+    # each collector, if specifies this, should actually implement a timeout
+    advertised_timeout: int | None = None
 
 
 def pack_checkbox_session(target_dir: Path, bug_report: BugReport) -> str:
@@ -61,19 +67,27 @@ def nvidia_bug_report(target_dir: Path, _: BugReport) -> str:
             str(target_dir / "nvidia-bug-report.log.gz"),
         ],
         text=True,
+        timeout=COMMAND_TIMEOUT,
     )
 
 
-def journal_of_past_week(target_dir: Path, _: BugReport) -> None:
-    with open(target_dir / "journalctl_1_week.log", "w") as f:
-        sp.check_call(
-            ["journalctl", "--since", "1 week ago"],
-            stdout=f,
-            text=True,
-        )
+def journal_logs(target_dir: Path, _: BugReport, num_days: int = 7) -> None:
+    filepath = target_dir / f"journalctl_{num_days}_days.log"
+    with open(filepath, "w") as f:
+        try:
+            sp.check_call(
+                ["journalctl", "--since", f"{num_days} days ago"],
+                stdout=f,
+                text=True,
+                timeout=COMMAND_TIMEOUT,
+            )
+        except sp.TimeoutExpired:
+            raise RuntimeError(
+                "Journalctl didn't finish dumping the logs in 600 seconds"
+            )
 
     bad = False
-    with open(target_dir / "journalctl_1_week.log") as f:
+    with open(filepath) as f:
         first_line = f.readline()
         if "No entries" in first_line:
             # this happens when the the journalctl binary can't read the
@@ -83,7 +97,7 @@ def journal_of_past_week(target_dir: Path, _: BugReport) -> None:
             bad = True
 
     if bad:
-        os.remove(target_dir / "journalctl_1_week.log")
+        os.remove(filepath)
         raise ValueError(
             "Not going to attach an empty journalctl file. "
             + "Is the DUT using a much newer version of journalctl?"
@@ -94,6 +108,7 @@ def acpidump(target_dir: Path, _: BugReport) -> str:
     return sp.check_output(
         ["acpidump", "-o", str(target_dir.absolute() / "acpidump.log")],
         text=True,
+        timeout=COMMAND_TIMEOUT,
     )
 
 
@@ -101,13 +116,24 @@ def dmesg_of_current_boot(target_dir: Path, _: BugReport) -> str:
     with open("/proc/sys/kernel/random/boot_id") as boot_id_file:
         boot_id = boot_id_file.read().strip().replace("-", "")
         with open(target_dir / f"dmesg-of-boot-{boot_id}.log", "w") as f:
-            sp.check_call(["dmesg"], stdout=f, stderr=sp.DEVNULL, text=True)
+            sp.check_call(
+                ["dmesg", "-T"],
+                stdout=f,
+                stderr=sp.DEVNULL,
+                text=True,
+                timeout=COMMAND_TIMEOUT,
+            )
             return f"Saved dmesg logs of boot {boot_id} to {f.name}"
 
 
 def snap_list(target_dir: Path, _: BugReport):
     with open(target_dir / "snap_list.log", "w") as f:
-        sp.check_call(["snap", "list", "--all"], stdout=f, text=True)
+        sp.check_call(
+            ["snap", "list", "--all"],
+            stdout=f,
+            text=True,
+            timeout=COMMAND_TIMEOUT,
+        )
 
 
 mock_collectors: Sequence[LogCollector] = (
@@ -128,12 +154,17 @@ mock_collectors: Sequence[LogCollector] = (
     ),
     LogCollector(
         "slow1",
-        lambda p, b: sp.check_output(["sleep", "6"], text=True),
+        lambda p, b: sp.check_output(
+            ["sleep", "60"], text=True, timeout=COMMAND_TIMEOUT
+        ),
         "Slow collect 1",
+        advertised_timeout=COMMAND_TIMEOUT,
     ),
     LogCollector(
         "slow2",
-        lambda p, b: sp.check_output(["sleep", "7"], text=True),
+        lambda p, b: sp.check_output(
+            ["sleep", "700"], text=True, timeout=COMMAND_TIMEOUT
+        ),
         "Slow collect 2",
     ),
     LogCollector(
@@ -152,11 +183,18 @@ mock_collectors: Sequence[LogCollector] = (
         "NVIDIA Bug Report",
     ),
     LogCollector(
-        "journalctl",
-        journal_of_past_week,
+        "journalctl-7-days",
+        lambda target_dir, bug_report: journal_logs(target_dir, bug_report, 7),
         "Journalctl Logs of This Week",
-        True,
+        False,
         'journalctl --since="1 week ago"',
+    ),
+    LogCollector(
+        "journalctl-3-days",
+        lambda target_dir, bug_report: journal_logs(target_dir, bug_report, 3),
+        "Journalctl Logs of the Last 3 Days",
+        True,
+        'journalctl --since="3 days ago"',
     ),
 )
 
@@ -175,6 +213,7 @@ real_collectors: Sequence[LogCollector] = (
         "dmesg Logs of This Boot",
         True,
         "sudo dmesg",
+        advertised_timeout=COMMAND_TIMEOUT,
     ),
     LogCollector(
         "checkbox-session",
@@ -187,13 +226,23 @@ real_collectors: Sequence[LogCollector] = (
         "NVIDIA Bug Report",
         False,
         "nvidia-bug-report.sh --extra-system-data",
+        advertised_timeout=COMMAND_TIMEOUT,
     ),
     LogCollector(
-        "journalctl",
-        journal_of_past_week,
-        "Journalctl Logs of This Week",
-        True,
+        "journalctl-7-days",
+        lambda target_dir, bug_report: journal_logs(target_dir, bug_report, 7),
+        "Journal Logs of This Week",
+        False,
         'journalctl --since="1 week ago"',
+        advertised_timeout=COMMAND_TIMEOUT,
+    ),
+    LogCollector(
+        "journalctl-3-days",
+        lambda target_dir, bug_report: journal_logs(target_dir, bug_report, 3),
+        "Journal Logs of the Last 3 Days",
+        True,
+        'journalctl --since="3 days ago"',
+        advertised_timeout=COMMAND_TIMEOUT,
     ),
     LogCollector(
         "snap-list",
@@ -201,6 +250,7 @@ real_collectors: Sequence[LogCollector] = (
         "List of Snaps in This System",
         True,
         "snap list --all",
+        advertised_timeout=COMMAND_TIMEOUT,
     ),
 )
 
