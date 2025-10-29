933fc904cc3dd7dc066ac428705246c7f5c81c00
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2025-10-29 14:25:36 +0800

    fix: remove duplicate function

--- src/bugit_v2/scripts/list_sessions.py
+++ src/bugit_v2/scripts/list_sessions.py
@@ -1,12 +1,11 @@
 import json
-import os
 import sys
 from pathlib import Path
 
 from rich import print as rich_print
 from typer import Typer
 
-from bugit_v2.checkbox_utils import Session
+from bugit_v2.checkbox_utils import Session, get_valid_sessions
 from bugit_v2.utils import is_prod, is_snap
 
 SESSION_ROOT_DIR = Path("/var/tmp/checkbox-ng/sessions")
@@ -19,25 +18,6 @@ app = Typer(
     no_args_is_help=True,
     add_completion=not is_snap(),  # the built-in ones doesn't work in snap
 )
-
-
-def get_valid_sessions() -> list[Path]:
-    """Get a list of valid sessions in /var/tmp/checkbox-ng
-
-    This is achieved by looking at which session directory has non-empty
-    io-logs. If it's empty, it's either tossed by checkbox or didn't even
-    reach the test case where it dumps the udev database, thus invalid
-    """
-    if not SESSION_ROOT_DIR.exists():
-        return []
-    valid_session_dirs: list[Path] = []
-    for d in os.listdir(SESSION_ROOT_DIR):
-        try:
-            if len(os.listdir(SESSION_ROOT_DIR / d / "io-logs")) != 0:
-                valid_session_dirs.append(SESSION_ROOT_DIR / d)
-        except FileNotFoundError:
-            continue
-    return valid_session_dirs
 
 
 @app.command("json", help="Print the info in JSON format")
