3362963908830d58f0c33907e7f2a88794c6a854
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2025-12-12 14:48:52 +0800

    fix: also trim length

--- src/bugit_v2/screens/bug_report_screen.py
+++ src/bugit_v2/screens/bug_report_screen.py
@@ -558,7 +558,9 @@ class BugReportScreen(Screen[BugReport]):
                     json.dump(d, f)
                 label.update("[green]Progress Saved")
             except Exception as e:
-                label.update(f"[red]Autosave failed! {escape_markup(repr(e))}")
+                label.update(
+                    f"[red]Autosave failed! {escape_markup(repr(e)[:20])}"
+                )
 
         # run auto save 0.5 seconds after the user stops typing
         self._debounce(lambda: self.run_worker(f, thread=True), 0.5)()
