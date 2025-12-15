7cd9d0a637db4aac35c5d66fb3a7e7bd3037e01e
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2025-12-15 11:40:11 +0800

    Change theme cmd (#101)
    * feat: save user theme preference
    
    * doc: changelog

--- src/bugit_v2/utils/constants.py
+++ src/bugit_v2/utils/constants.py
@@ -83,8 +83,15 @@ AUTOSAVE_DIR = (
     Path(os.getenv("SNAP_DATA", str(Path.home().absolute() / ".cache")))
     / "bugit-v2-autosave"
 )
+VISUAL_CONFIG_DIR = (
+    Path(os.getenv("SNAP_DATA", str(Path.home().absolute() / ".config")))
+    / "bugit-v2-visual-config"
+)
+
 if not AUTOSAVE_DIR.exists():
     os.makedirs(AUTOSAVE_DIR, exist_ok=True)
+if not VISUAL_CONFIG_DIR.exists():
+    os.makedirs(VISUAL_CONFIG_DIR, exist_ok=True)
 
 
 class NullSelection(enum.Enum):
