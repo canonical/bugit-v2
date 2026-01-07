18b40faffedd2b59881e9d73dbe3dbbfda0ca578
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2026-01-07 17:26:50 +0800

    fix: temp remove cache

--- src/bugit_v2/checkbox_utils/get_cert_status.py
+++ src/bugit_v2/checkbox_utils/get_cert_status.py
@@ -8,8 +8,6 @@ from pathlib import Path
 from sys import stderr
 from typing import NamedTuple
 
-from async_lru import alru_cache
-
 from bugit_v2.checkbox_utils.checkbox_exec import checkbox_exec
 from bugit_v2.checkbox_utils.checkbox_session import SESSION_ROOT_DIR
 from bugit_v2.checkbox_utils.models import CERT_STATUSES, CertificationStatus
@@ -95,7 +93,6 @@ def get_session_envs(session_path: Path) -> dict[str, str]:
     return out
 
 
-@alru_cache
 async def get_certification_status(
     test_plan: str, session_path: Path | None = None
 ) -> dict[str, TestCaseWithCertStatus]:
