3280f25531d313faf65ccfab2eae794e70a2586f
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2026-01-08 11:50:04 +0800

    fix: unnecessary wrapper

--- src/bugit_v2/scripts/dump_standard_info.py
+++ src/bugit_v2/scripts/dump_standard_info.py
@@ -4,7 +4,6 @@ Generates the "Additional Information" section from the bug report
 
 import asyncio
 import json
-from functools import wraps
 from typing import Annotated
 
 import typer
@@ -24,19 +23,10 @@ app = typer.Typer(
 )
 
 
-def syncify(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        return asyncio.run(f(*args, **kwargs))
-
-    return wrapper
-
-
 @app.command(
     help="(sudo required) Print the info in a human-friendly format. Pipe the output to 'cat' to remove colors.",
 )
-@syncify
-async def main(
+def main(
     print_json: Annotated[
         bool, typer.Option("--json", help="Print in JSON format")
     ] = False,
@@ -46,9 +36,9 @@ async def main(
 ):
     sudo_devmode_check()
     if no_timeout:
-        info = await get_standard_info(None)
+        info = asyncio.run(get_standard_info(None))
     else:
-        info = await get_standard_info()
+        info = asyncio.run(get_standard_info())
     saved_dut_info = get_saved_dut_info()
 
     if saved_dut_info:
