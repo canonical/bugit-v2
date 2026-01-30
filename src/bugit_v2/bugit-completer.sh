1f8c0bbbd34c75b70660065af4d3648377d67e0d
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2026-01-30 14:57:48 +0800

    feat: make completer compatible with new snapd

--- src/bugit_v2/bugit-completer.sh
+++ src/bugit_v2/bugit-completer.sh
@@ -3,11 +3,8 @@
 _bugit_v2_completion() {
     local IFS=$'
 '
-    COMPREPLY=( $( env COMP_WORDS="${COMP_WORDS[*]}" \
-                   COMP_CWORD=$COMP_CWORD \
-                   _BUGIT_V2_COMPLETE=complete_bash python3 $SNAP/src/bugit_v2/app.py ) )
+    COMPREPLY=( $(env COMP_WORDS="${COMP_WORDS[*]}" COMP_CWORD=$COMP_CWORD _BUGIT.BUGIT_V2_COMPLETE=complete_bash python3 $SNAP/src/bugit_v2/app.py) )
     return 0
 }
-
 
 complete -o default -F _bugit_v2_completion bugit.bugit-v2
