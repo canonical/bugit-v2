d980ef9a804eedf7fd8ebc1592316b56e7fb271d
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2026-04-23 11:37:56 +0800

    refactor: combine entry points

--- src/bugit_v2/bug_report_submitters/local_file_submitter.py
+++ src/bugit_v2/bug_report_submitters/local_file_submitter.py
@@ -1,23 +1,58 @@
 from collections.abc import Generator
+from dataclasses import asdict
+import json
 from pathlib import Path
-from typing import override
+import shutil
+from tempfile import TemporaryDirectory
+from typing import final, override
 from bugit_v2.bug_report_submitters.bug_report_submitter import (
     AdvanceMessage,
     BugReportSubmitter,
 )
-from bugit_v2.models.bug_report import BugReport
+from bugit_v2.models.bug_report import SEVERITIES, BugReport
 
 
+"""
+A local .gz file should have
+
+bug-report.gz
+    bug-report.txt
+    attachment1.tar
+    attachment2.tar
+    checkbox-session.tar
+
+
+"""
+
+
+@final
 class LocalFileSubmitter(BugReportSubmitter[None]):
+    name = "local_file_submitter"
+    display_name = "Local File Submitter"
+    severity_name_map = {sev: sev for sev in SEVERITIES}
+    steps = 1
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.working_dir = TemporaryDirectory()
+
+    def __del__(self):
+        self.working_dir.cleanup()
+
     @override
     def submit(
         self, bug_report: BugReport
     ) -> Generator[str | AdvanceMessage, None, None]:
-        return super().submit(bug_report)
+        report_json_path = Path(self.working_dir.name) / "bug-report.json"
+        with open(report_json_path, "w") as f:
+            d = asdict(bug_report, dict_factory=BugReport.dict_factory)
+            json.dump(d, f)
+
+        yield f"Dumped bug report to {report_json_path}"
 
     @override
     def upload_attachment(self, attachment_file: Path) -> str | None:
-        return super().upload_attachment(attachment_file)
+        shutil.copy(attachment_file, self.working_dir.name)
 
     @override
     def bug_exists(self, bug_id: str) -> bool:
