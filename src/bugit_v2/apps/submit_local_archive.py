5c3b4ca55c6c60061e569654aa275cce9e4b984d
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2026-05-05 13:55:16 +0800

    Don't run log collecters inside submit (#195)
    * fix: don't run log collecters inside submit
    
    * fix: check if path is dir
    
    * refactor: combine entry points
    
    * fix: debug msg
    
    * fix: description
    
    * perf: use symlink not copy

--- src/bugit_v2/apps/submit_local_archive.py
+++ src/bugit_v2/apps/submit_local_archive.py
@@ -1,22 +1,31 @@
 import json
-from pathlib import Path
+import shutil
 import tarfile
+from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import Any, final
+
+import typer
 from textual import work
 from textual.app import App
 from textual.driver import Driver
 from textual.types import CSSPathType
 from typing_extensions import Annotated
-import typer
 
 from bugit_v2.bug_report_submitters.bug_report_submitter import BugReportSubmitter
 from bugit_v2.bug_report_submitters.jira_submitter import JiraSubmitter
 from bugit_v2.bug_report_submitters.launchpad_submitter import LaunchpadSubmitter
 from bugit_v2.bug_report_submitters.local_file_submitter import SERIALIZED_REPORT_NAME
+from bugit_v2.bug_report_submitters.mock_jira import MockJiraSubmitter
+from bugit_v2.bug_report_submitters.mock_lp import MockLaunchpadSubmitter
 from bugit_v2.models.bug_report import BugReport, SerializableBugReport
 from bugit_v2.screens.submission_progress_screen import SubmissionProgressScreen
 from bugit_v2.utils import is_prod, is_snap
+
+# these files or directories in the working_dir should not be uploaded to jira/lp
+ATTACHMENT_BLACKLIST = [
+    SERIALIZED_REPORT_NAME,
+]
 
 app = typer.Typer(
     context_settings={"help_option_names": ["-h", "--help"]},
@@ -34,6 +43,7 @@ class SubmitOnlyApp(App[None]):
         self,
         report: BugReport,
         submitter: BugReportSubmitter[Any],
+        attachment_dir: Path,
         # ---
         driver_class: type[Driver] | None = None,
         css_path: CSSPathType | None = None,
@@ -43,11 +53,17 @@ class SubmitOnlyApp(App[None]):
         super().__init__(driver_class, css_path, watch_css, ansi_color)
         self.report = report
         self.submitter = submitter
+        self.attachment_dir = attachment_dir
 
     @work
     async def on_mount(self):
         await self.push_screen_wait(
-            SubmissionProgressScreen(self.report, self.submitter, mode="app"),
+            SubmissionProgressScreen(
+                bug_report=self.report,
+                submitter=self.submitter,
+                attachment_dir=self.attachment_dir,
+                mode="app",
+            ),
         )
         self.exit()
 
@@ -82,38 +98,16 @@ def build_bug_report_from_archive(file: Path, working_dir: Path) -> BugReport:
             return serialized_report.to_bug_report()
 
 
-@app.command("jira", help="Submit to Jira")
-def jira_main(
-    file: Annotated[
-        Path,
-        typer.Argument(
-            help=(
-                "The .tar.gz file created by [u]bugit-v2 local[/]. "
-                + "A jira bug report will be created based on the content of this archive."
-            ),
-            exists=True,
-            dir_okay=False,
-            file_okay=True,
-            readable=True,
-            resolve_path=True,
-        ),
-        build_bug_report_from_archive,
-    ],
-):
-    with TemporaryDirectory() as temp_dir_str:
-        report = build_bug_report_from_archive(file, Path(temp_dir_str))
-        submitter = JiraSubmitter()
-        SubmitOnlyApp(report, submitter).run()
-
-
 @app.command("lp", help="Submit to Launchpad")
-def lp_main(
+@app.command("jira", help="Submit to Jira")
+def main(
+    ctx: typer.Context,
     file: Annotated[
         Path,
         typer.Argument(
             help=(
                 "The .tar.gz file created by [u]bugit-v2 local[/]. "
-                + "A launchpad bug report will be created based on the content of this archive."
+                + "A Jira/Launchpad bug report will be created based on the content of this archive."
             ),
             exists=True,
             dir_okay=False,
@@ -123,10 +117,38 @@ def lp_main(
         ),
     ],
 ):
-    with TemporaryDirectory() as temp_dir_str:
-        report = build_bug_report_from_archive(file, Path(temp_dir_str))
-        submitter = LaunchpadSubmitter()
-        SubmitOnlyApp(report, submitter).run()
+    match ctx.command.name:
+        case "lp":
+            submitter = LaunchpadSubmitter() if is_prod() else MockLaunchpadSubmitter()
+        case "jira":
+            submitter = JiraSubmitter() if is_prod() else MockJiraSubmitter()
+        case _:
+            typer.echo(f"Unexpected command '{ctx.command.name}'")
+            raise typer.Exit(1)
+
+    with (
+        TemporaryDirectory() as temp_extract_dir_str,
+        TemporaryDirectory() as temp_attachment_dir_str,
+    ):
+        temp_extract_dir = Path(temp_extract_dir_str)
+        temp_attachment_dir = Path(temp_attachment_dir_str)
+
+        report = build_bug_report_from_archive(file, temp_extract_dir)
+
+        for file in temp_extract_dir.iterdir():
+            if not file.is_file():
+                continue
+            if file.name in ATTACHMENT_BLACKLIST:
+                continue
+            if (
+                file.name == "checkbox_session.tar.gz"
+                and "checkbox-session" not in report.logs_to_include
+            ):
+                continue
+
+            (temp_attachment_dir / file.name).symlink_to(file)
+
+        SubmitOnlyApp(report, submitter, temp_attachment_dir).run()
 
 
 if __name__ == "__main__":
